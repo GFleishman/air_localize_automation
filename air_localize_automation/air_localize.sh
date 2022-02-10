51475aa2f1ab57ecf3ddcce5c1403b31f1bc5999
fleishmang <fleishmang@janelia.hhmi.org>
2022-02-10 09:55:38 -0500

    consolidated shell scripts, removed duplicate

--- air_localize_automation/air_localize.sh
+++ air_localize_automation/air_localize.sh
@@ -47,6 +47,44 @@ function initialize_environment {
 }
 
 
+# the image from which to find spots
+image="/groups/multifish/multifish/Yuhan/Stitch/CEA_3_R10/Stitch/n5"
+# the folder where you'd like all outputs to be written
+outdir="/groups/multifish/multifish/fleishmang/alignments/spots_CEA_3_R10"
+
+
+# the channels from which to find spots
+channel="c0,c1,c3"
+# the scale level at which to find spots
+scale="s0"
+# the number of voxels along x/y for tiling
+xy_stride=1024
+# the number of voxels to overlap along x/y between tiles
+xy_overlap=51
+# the number of voxels along z for tiling
+z_stride=256
+# the number of voxels to overlap along z between tiles
+z_overlap=26
+# the channel with DAPI
+dapi_channel="c2"
+# channels needing DAPI bleedthrough correction
+dapi_correction_channels="c3"
+# DAPI bleedthrough percentage
+dapi_percentage="0.24"
+
+# the air localize parameters file
+# the numerical parameters related to air localize are found in this file
+# TODO: LET'S ALLOW FOR DIFFERENT PARAMETER FILE FOR EACH SPOTS CHANNEL
+params="/groups/multifish/multifish/fleishmang/air_localize/air_localize_default_params.txt"
+
+
+# DO NOT EDIT BELOW THIS LINE
+air_localize='/groups/multifish/multifish/fleishmang/air_localize/air_localize.sh'
+bash "$air_localize" "$image" "$channel" "$scale" "$xy_stride" \
+     "$xy_overlap" "$z_stride" "$z_overlap" "$params" "$outdir" \
+     "$dapi_channel" "$dapi_correction_channels" "$dapi_percentage"
+
+
 image=${1?}; shift
 channels=${1?}; shift
 scale=${1?}; shift
